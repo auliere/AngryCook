f5d8283c9b122090b84b0ed245da7f40b446fbd5
auliere <auliere>
2015-11-28 22:33:00 +0200

    Initial commit

---
+++ Assets/Scripts/EnemyActionScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyActionScript : MonoBehaviour {
+    public float hungerLevel;
+    public float hungerDelta2;
+    public float demage;
+    public float speed = 1;
+
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = player.transform.position.x - gameObject.transform.position.x;
+        float deltaY = player.transform.position.y - gameObject.transform.position.y;
+        float deltaz = player.transform.position.z - gameObject.transform.position.z;
+
+        if (deltaX * deltaX + deltaY * deltaY + deltaz * deltaz <= 1000)
+        {
+            Move();
+            //Debug.Log("in range");
+        }
+        //else
+           // Debug.Log("not in range");
+    }
+
+    void Move ()
+    {
+        //Debug.Log("Moving");
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider collider) {
+          switch(collider.gameObject.tag)
+          {
+            case "Bullet":
+                float damage = collider.gameObject.GetComponent<BulletController>().damage;
+                Debug.Log("Decreasing hunger");
+                hungerLevel -= damage;
+				//Destroy(collider.gameObject);
+				if (hungerLevel <= 0)
+                {
+                    Debug.Log("Enemy is dead");
+                    Destroy(gameObject);
+                }
+                break;
+            case "Player":
+                var controller = collider.gameObject.GetComponentInChildren<PlayerController>();
+                controller.health -= demage;
+                if (controller.health <= 0)
+                {
+                    Destroy(player);
+                    Debug.Log("Game Over, you are lost");
+                }
+                break;
+         }
+    }
+
+ }
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+    System.Random rand = new System.Random();
+
+    int minZ = 5;
+    int maxZ = 10;
+    int minX = 5;
+    int maxX = 10;
+
+    public GameObject player;
+    public GameObject enemy;
+
+    ArrayList enemies = new ArrayList();
+    int numberOfEnemies = 0;
+    int maxAmountOfEnemies = 20;
+    Animator anim;
+
+    ArrayList spawnPoints = new ArrayList() { };
+
+	// Use this for initialization
+	void Start ()
+    {
+//        player = GameObject.FindGameObjectWithTag("Player");
+        numberOfEnemies = rand.Next(10, 20);
+        for (int i = 2; i < numberOfEnemies+2; i++)
+            Spawn(i);
+    }
+
+    void Spawn(int i) {
+        if (enemies.Count >= maxAmountOfEnemies)
+            return;
+        var enemy = Instantiate(this.enemy);
+        
+        enemy.transform.position = new Vector3(
+            player.transform.position.x + rand.Next(minX, maxX) * Mathf.Cos(i), 
+            player.transform.position.y, player.transform.position.z + rand.Next(minX, maxX) * Mathf.Sin(i));
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
